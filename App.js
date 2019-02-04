906f37640db6879c389d870eb4051ca198252565
SanduSuresh <sandeepsureshbacks@gmail.com>
2019-02-04 14:15:24 +0530

    initial commit

--- App.js
+++ App.js
@@ -7,7 +7,12 @@ import LoginScreen from "./Screens/LoginScreen";
 import UserSignupScreen from "./Screens/UserSignupScreen";
 import HandymanSignupScreen from "./Screens/HandymanSignupScreen";
 import RegisterSelection from "./Screens/RegisterSelection";
-
+import ElectricianScreen from "./Screens/ElectricianScreen";
+import PlumberScreen from "./Screens/PlumberScreen";
+import PainterScreen from "./Screens/PainterScreen";
+import GardenerScreen from "./Screens/GardenerScreen";
+import CarpenterScreen from "./Screens/CarpenterScreen";
+import CleanerScreen from "./Screens/CleanerScreen";
 
 export default class App extends React.Component {
   render() {
@@ -52,7 +57,43 @@ const AppNavigator = createStackNavigator(
       navigationOptions: () => ({
         header: null
       })
-    },    
+    },  
+    Electrician: {
+      screen: ElectricianScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    },   
+    Plumber: {
+      screen: PlumberScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    Painter: {
+      screen: PainterScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    }, 
+    Gardener: {
+      screen: GardenerScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    Carpenter: {
+      screen: CarpenterScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    Cleaner: {
+      screen: CleanerScreen,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
   },
   {
     initialRouteName: "Login"
---
+++ Screens/ElectricianScreen.js
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  StyleSheet,  
+  View,
+  ImageBackground,
+  Text
+} from "react-native";
+import CheckBox from 'react-native-check-box';
+
+export default class ElectricianScreen extends React.Component{
+    constructor() {
+        super();
+        this.state={
+            isChecked1: false,
+            isChecked2: false,
+            isChecked3: false,
+            isChecked4: false,
+            isChecked5: false,
+        };
+    }
+    
+    render(){
+        return(
+            <ImageBackground
+               source={require("../assets/background/bgwatermark.jpg")}
+               style={styles.container}
+            >
+                <View style={styles.container}>
+                <Text style={styles.title}>Electrician</Text>
+                <View style={styles.checkContainer}>
+                    <CheckBox style={styles.checkboxstyle}
+                        onClick={()=>{
+                            this.setState({
+                                isChecked1:!this.state.isChecked1
+                            })
+                          }}
+                          isChecked={this.state.isChecked1}
+                          leftTextStyle={styles.checkboxtext}
+                        leftText={"General Electric Works"}
+                    />  
+                   <CheckBox style={styles.checkboxstyle}
+                        onClick={()=>{
+                            this.setState({
+                                isChecked2:!this.state.isChecked2
+                            })
+                          }}
+                          isChecked={this.state.isChecked2}
+                          leftTextStyle={styles.checkboxtext}
+                         leftText={"Electrical Wiring"}
+                    />  
+                    <CheckBox style={styles.checkboxstyle}
+                        onClick={()=>{
+                            this.setState({
+                                isChecked3:!this.state.isChecked3
+                            })
+                          }}
+                          isChecked={this.state.isChecked3}
+                          leftTextStyle={styles.checkboxtext}
+                        leftText={"Motor Repairing"}
+                    /> 
+                    <CheckBox style={styles.checkboxstyle}
+                        onClick={()=>{
+                            this.setState({
+                                isChecked4:!this.state.isChecked4
+                            })
+                          }}
+                          isChecked={this.state.isChecked4}
+                          leftTextStyle={styles.checkboxtext}
+                        leftText={"Inverter Servicing or Repair"}
+                    /> 
+                    <CheckBox style={styles.checkboxstyle}
+                        onClick={()=>{
+                            this.setState({
+                                isChecked5:!this.state.isChecked5
+                            })
+                          }}
+                          isChecked={this.state.isChecked5}
+                          leftTextStyle={styles.checkboxtext}
+                        leftText={"Others"}
+                    />
+                </View>
+            </View>
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        height: '100%',
+        flex:1,
+    },
+    title: {
+        fontSize: 25,
+        textAlign: 'center',
+        color: '#ffffff',
+        marginTop: 20,
+        fontWeight: 'bold',
+        padding: 20
+    },
+    checkboxstyle:{
+        padding: 20,
+    },
+    checkContainer: {
+        flexDirection: "column",        
+    },
+    checkboxtext: {
+        color: '#ffffff',
+        fontWeight: 'bold'
+    } 
+})
